0d2590912f9dac40cbcdaf5a50487bc6e27476da
shiraori11 <senpaiyui69@gmail.com>
2026-05-19 11:57:37 +0800

    Added solution to /foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+  function checkArrayItemToNum(item, num) {
+    return item !== num;
+  }
+ 
+  for (const n of num) {
+    arr = arr.filter(item => item !== n)
+  }
+
+  return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
