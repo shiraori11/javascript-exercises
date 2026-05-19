1a91f969ba7176ae052fd10598afb6c118829aca
shiraori11 <senpaiyui69@gmail.com>
2026-05-19 08:54:19 +0800

    Added solution to /foundations/06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+function repeatString(string, num){
+  let repeatedString = "";
+  
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      repeatedString += string;
+    }
+  }
 
-};
+  return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
