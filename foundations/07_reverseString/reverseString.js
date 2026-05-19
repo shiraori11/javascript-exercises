67a7629f59e26b5a8e27048c827de93997032f49
shiraori11 <senpaiyui69@gmail.com>
2026-05-19 10:35:39 +0800

    Added solution to /foundations/07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  reversedString = "";
+  lengthOfString = string.length;
+  for (let i = lengthOfString - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
 
+  return reversedString;
 };
 
 // Do not edit below this line
