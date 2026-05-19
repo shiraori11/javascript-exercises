8d861b15796a1a51eef76290ac2842eb30ca494b
shiraori11 <senpaiyui69@gmail.com>
2026-05-19 14:40:55 +0800

    Added solution to /foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let lowestNum;
+  let highestNum;
+  let totalSum = 0;
+  
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+    return "ERROR";
+  } else if (num1 < num2){
+    lowestNum = num1;
+    highestNum = num2;
+  } else {
+    lowestNum = num2;
+    highestNum = num1;
+  }
 
+  for (lowestNum; lowestNum <= highestNum; lowestNum++) {
+    totalSum += lowestNum;
+  }
+
+  return totalSum;
 };
 
 // Do not edit below this line
