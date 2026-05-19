694e89d6fc0dbe387efd2baf5c089d9422c469af
shiraori11 <senpaiyui69@gmail.com>
2026-05-19 18:23:09 +0800

    Added solution to /foundations/10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if (year % 4 === 0) {
+    if (year % 400 !== 0 && year % 100 === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
+  return false;
 };
 
 // Do not edit below this line
