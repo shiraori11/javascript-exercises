e91f6ace483870f9205d16ffb1d24bbb860239c5
shiraori11 <shiraori@proton.me>
2026-05-23 14:04:24 +0800

    Added a solution to /foundations/12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0){
+    return 0;
+  }
+	return arr.reduce((accumulator, currentSum) => accumulator + currentSum);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
 
+  return arr.reduce((accumulator, currentProduct) => accumulator * currentProduct);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorialProduct = 1;
+	for (let i = 0; i <= num; i++) {
+	  if (i > 1) {
+	    factorialProduct = factorialProduct * i;
+	  }
+	}
+
+	return factorialProduct;
 };
 
 // Do not edit below this line
