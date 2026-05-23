2243b1978aae3d12ea40c79a3cb5d389359ff040
shiraori11 <shiraori@proton.me>
2026-05-23 19:15:29 +0800

    Added solution to /foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  
+  if (parseInt(num) === 0) {
+    return 0;
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+  
+  let currentFibo = 1;
+  let previousNum  = 0;
+  for (let i = 1; i < num; i++) {
+    throwawayNum = currentFibo;
+    currentFibo = previousNum + currentFibo;
+    previousNum = throwawayNum;
+    console.log(currentFibo);
+  }
 
+  return currentFibo;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// currentSum = 1 1 2 3 5 8 13 
+// previous = 1 1 2 3 5 8
