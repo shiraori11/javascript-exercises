396ad770945744cb16e2cf3ac3b45ae70eed5b9e
shiraori11 <shiraori@proton.me>
2026-05-23 17:24:05 +0800

    Added a solution to /foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  let cleanText = text.replace(".", "").replace("!", "").toLowerCase();
+  cleanText = cleanText.split(" ").join("");
+  cleanText = cleanText.split(",").join("");
+  console.log(cleanText);
+  let counterForText = cleanText.length - 1;
 
+  for (let c = 0; c < cleanText.length; c++) {
+    if (cleanText[c] !== cleanText[counterForText]) {
+      return false;
+    }
+    counterForText--;
+  }
+  return true;
 };
 
 // Do not edit below this line
