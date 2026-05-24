6bc749c0d04042f3b3957da25637cbc86a661b53
shiraori11 <shiraori@proton.me>
2026-05-25 05:18:15 +0800

    Added solution to foundations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestPerson;
 
+  for (person of people) {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    
+    if (oldestPerson === undefined) {
+      oldestPerson = person;
+    } else {
+      oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+
+      personAge = person.yearOfDeath - person.yearOfBirth;
+      console.log(oldestPersonAge);
+      console.log(personAge);
+
+      if (oldestPersonAge < personAge) {
+        oldestPerson = person;
+      }
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
